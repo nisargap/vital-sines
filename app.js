5716727df8349180982bc427d4f886cb1f4e0b06
Robert Rose <bertra97@gmail.com>
2015-11-11 19:20:07 -0500

    Made changes to support auth development
    Made some changes to work on auth dev.
    - Removed results.js (obsolete)
    - Removed results.ejs (obsolete)
    - Removed references to results from app.js
    - Added part of the code for login via GitHub.
    - Using Parse demo code:
    https://github.com/ParsePlatform/CloudCodeOAuthGitHubTutorial/blob/master/cloud/main.js

--- app.js
+++ app.js
@@ -4,14 +4,44 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
-// var multipart = require('multipart');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var results = require('./routes/results.js');
+var multer = require('multer');
+var keys = require('../creds/creds');
+
 
 var app = express();
-var multer = require('multer');
+
+
+/**********************
+ * For authentication
+ */
+var Parse = require('parse/node').Parse;
+var parseKeys = require('../creds/creds');
+Parse.initialize(parseKeys.parse_id, parseKeys.parse_js);
+var querystring = require('querystring');
+var _ = require('underscore');
+var Buffer = require('buffer').Buffer;
+
+var githubClientId = 'your-github-client-id-here';
+var githubClientSecret = 'your-github-client-secret-here';
+
+// Create resticted user rules for parse object.
+var restrictedACL = new Parse.ACL();
+restrictedACL.setPublicReadAccess(false);
+restrictedACL.setPublicWriteAccess(false);
+
+// Disable public access for Get/Find/Create/Update/Delete. Master key only.
+var TokenRequest = Parse.Object.extend("TokenRequest");
+var TokenStorage = Parse.Object.extend("TokenStorage");
+
+var githubRedirectEndpoint = 'https://github.com/login/oauth/authorize?';
+var githubValidateEndpoint = 'https://github.com/login/oauth/access_token';
+var githubUserEndpoint = 'https://api.github.com/user';
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,7 +52,7 @@ app.use(bodyParser({keepExtensions:true,uploadDir:path.join(__dirname,'/files')}
 
 app.use(multer({dest:'./uploads/'}).single('genome-file'));
 app.use(multer({dest:'./uploads/'}).single('eye-image'));
-// uncomment after placing your favicon in /public
+// TODO: uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -33,7 +63,6 @@ app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/results', results);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -66,5 +95,52 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// Authentication code was adapted from: https://github.com/ParsePlatform/CloudCodeOAuthGitHubTutorial/blob/master/cloud/main.js
+app.get('/authorize', function(req, res) {
+  
+  var tokenRequest = new Parse.Object("TokenRequest");
+  // Secure against public
+  tokenRequest.setACL(restrictedACL);
+  
+  tokenRequest.save(null, {useMasterKey: true}).then(function(obj) {
+    // Redirect browser for authorization.
+    
+    res.redirect(
+      githubRedirectEndpoint + querystring.stringify({
+        client_id: githubClientId,
+        state: obj.id
+  })
+    );
+  }, function(error) {
+    res.render('error', {errorMessage: 'Failed to save auth request.'});
+  });
+});
+
+app.get('/oauthCallback', function(req, res) {
+  var data = req.query;
+  var token;
+  
+  if(!(data && data.code && data.state)) {
+    res.render('error', { errorMessage: 'Invalid auth response received.'});
+    return;
+  }
+  var query = new Parse.Query(TokenRequest);
+  
+  Parse.Cloud.useMasterKey();
+  Parse.Promise.as().then(function() {
+    return query.get(data.state);
+  }).then(function() {
+    return obj.destroy();
+  }).then(function() {
+    return getGitHubAccessToken(data.code);
+  }).then(funtion(access) {
+    var githubData = access.data;
+    if(githubData && githubData.access_token && githubData.token_type) {
+      token = githubData.access_token;
+    } else {
+      return Parse.Promise.error("Invalid acess request.");
+    }
+  }); //TODO: Finish coding. I'm currently on line 153 of https://github.com/ParsePlatform/CloudCodeOAuthGitHubTutorial/blob/master/cloud/main.js
+});
 
 module.exports = app;
